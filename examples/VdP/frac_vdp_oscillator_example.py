Several bug fixes for Hilfer fractional derivative systems

examples/VdP/frac_vdp_oscillator_example.py
@@ -3,7 +3,7 @@ import fracnum as fr
 
 from fracnum.splines import BernsteinSplines
 from fracnum.plotting_utils import VdP_Plotter
-
+from fracnum.numerical import build_hilf_knot_vals
 
 """
 Example file of using Bernstein splines for time-integrating the (forced) fractional Van der Pol oscillator in the sense:
@@ -21,6 +21,7 @@ Where D represents the Caputo derivative of order alpha. Enjoy the stability and
 ######################
 
 alpha_damping = 0.8     # Fractional order of damping
+beta = 0.5              # Hilfer Beta
 
 params = {
     'mu': 1     # mu parameter of VdP oscillator
@@ -29,8 +30,8 @@ params = {
 # Forcing is a list of dictionaries applying A*sin(omega*t) + c to solution component dim (1 gives y in this case)
 forcing_params = [{
     'dim' : 1,
-    'A' : 3,
-    'omega' : 2,
+    'A' : 3, #3
+    'omega' : 3.3,#1.94,#4.0,#6.2,
     'c' : 0
 }]
 
@@ -42,17 +43,24 @@ VdP_bs = fr.ode_functions.VdP(params, bernstein = True, transpose = False)
 # Spline settings #
 ###################
 
-T = 200                 # Integration max time
+### Knot input values ###
+T = np.pi*10           # Integration max time
+eps = 10**(-15)         # Time shift epsilon, start of interval
 dt = 0.05               # Spline size (also called h) though varying size can also be used by creating a custom t_knot_vals
-N_knot_int = int(T/dt)  # Number of knots
-n_eval = 1              # Polynomial order. NOTE: when taking the spline derivative as done below, it is beneficial to keep this at 1 for plotting / spline cont. diff reasons
+c = 3/2                 # Knot size increase constant
+##
+
+gamm = alpha_damping + beta - alpha_damping*beta # Hilfer gamma parametrization
+
+t_knot_vals = build_hilf_knot_vals(eps, T, c, gamm, dt) # np.linspace(0, T, N_knot_int+1)  # Initialize equidistant knot values [t_0, t_1, t_2 ... t_k]. Can be generalized to anything.
 
 ####
+
+n_eval = 1 # Polynomial order
+
 # A bit of a technicality: the order of the polynomial goes up by 3 since it gets multiplied 3 times in the VdP system equation.
 # In general equal to m * n_eval, where m denotes the highest order of multiplication in x-components of f(x)
 n_calc = VdP_bs.N_upscale * n_eval
-
-t_knot_vals = np.linspace(0, T, N_knot_int+1)  # Initialize equidistant knot values [t_0, t_1, t_2 ... t_k]. Can be generalized to anything.
 
 bs = BernsteinSplines(t_knot_vals, n_calc, n_eval=n_eval)  # Initialize splines setup!
 
@@ -66,32 +74,34 @@ VdP_bs.set_bs_mult_upscale_functions(mult, upscale)
 ############
 
 # Initial value (x, y).
-# NOTE: for now, keep y to zero so that the derivative calculation still applies without need for a constant
 x_0 = np.array([2, 0])
 
 # alpha for x and y component separately. For reference, see https://doi.org/10.1016/j.chaos.2006.05.010
 system_alpha_vals = [alpha_damping, 2-alpha_damping]    
 # Initialize the solving structure
-spline_solver = bs.initialize_solver(VdP_bs.f, x_0, system_alpha_vals, forcing_params = forcing_params) 
+spline_solver = bs.initialize_solver(VdP_bs.f, x_0, system_alpha_vals, beta_vals = beta, forcing_params = forcing_params) 
 # Run the solver. method = 'local' uses knot-by-knot integration, method = 'global' at-large interval integration.
-results = spline_solver.run(method='local', verbose = False)
+results = spline_solver.run(method='local', verbose = False, t_eval=t_knot_vals)
 
 # Save some results...
-x = results['x']                      # Function at evaluation points as vector
+x = results['x'][0]                   # Function at evaluation points as vector
 a_vals = results['a']                 # Splines coefficients in matrix form
 comp_time = results['total_time']     # Total computational time
 t = results['t']                      # Knot time values
 
-x_der = bs.ddt(a_vals[0, :, :], upscale = True) # Calculate the derivative of x
+x_der = np.diff(x)/np.diff(t) # Calculate the derivative of x
 
 ############
 # Plotting #
 ############
 
-# Last value is discarded for the derivative calculation
-plot_x = x[:-1, 0]
-plot_x_der = x_der[:-1]
-plot_t = t[:-1]
+skip_n_vals = 10 # Skip first n vals in case of Hilfer derivative for plotting
+if np.all(beta == 1) :
+    skip_n_vals = 0
+
+plot_x = x[skip_n_vals:-1] # Last value is discarded for the derivative calculation
+plot_x_der = x_der[skip_n_vals:]
+plot_t = t[skip_n_vals:-1]
 
 # If provided, can be used to keep fixed limits for the frame. Currently not inputted below.
 lims_override = {
@@ -111,18 +121,18 @@ plot_object = VdP_Plotter(
     n_eval, 
     comp_time, 
     forcing_params=forcing_params, 
-    lims_override = None
+    lims_override = None,
+    beta = beta
 )
 
 plot_object.phase()
-plot_object.signal(hd_aspect=True)
+plot_object.phase(empty = True)
 plot_object.threedee()
 plot_object.fourier_spectrum()
 plot_object.phase_fourier()
 
 plot_object.show_plots()
 
-breakpoint()
 ############
 # Enjoy!:) #
 ############

fracnum/ode_functions.py
@@ -28,6 +28,7 @@ class VdP(ODEFunctions):
 
         mu = self.params['mu']
         if self.bernstein:
+            # breakpoint()
             A_x_in, A_y_in = x_vals[0, :], x_vals[1, :]
 
             if self.three_system == False:
@@ -73,7 +74,72 @@ class VdP(ODEFunctions):
                     y_out+=c
 
             return np.array([x_out, y_out]).T
-        
+# class VdP(ODEFunctions):
+#     def __init__(self, params = {}, bernstein=False, transpose = False):
+#         super().__init__(params, bernstein, transpose)
+#         self.N_upscale = 3
+#         self.three_system = False
+#         self.alpha_zero = False
+
+#     def f(self, t_vals, x_vals):
+#         if self.transpose:
+#             x_vals = x_vals.T
+
+#         mu = self.params['mu']
+#         if self.bernstein:
+#             A_x_in, A_y_in = x_vals[0, :], x_vals[1, :]
+
+#             if self.three_system == False:
+#                 if self.alpha_zero == False:
+#                     # x' = y
+#                     A_x_out = self.bs_upscale(A_y_in, self.N_upscale)
+
+#                     # y' = mu * (1-x^2)y - x
+#                     A_y_out = mu * self.bs_mult(1 - self.bs_mult(A_x_in, A_x_in), A_y_in) - self.bs_upscale(A_x_in, self.N_upscale)
+#                 else:
+#                     # x' = y
+#                     A_x_out = self.bs_upscale(A_y_in, self.N_upscale)
+
+#                     # y' = mu * (1-x^2)y - x
+#                     A_y_out = mu * self.bs_mult(1 - self.bs_mult(A_x_in, A_x_in), A_x_in) - self.bs_upscale(A_x_in, self.N_upscale)
+
+#                 return np.array([A_x_out, A_y_out])
+#             else:
+#                 A_z_in = x_vals[2, :]
+
+#                 # x' = z
+#                 A_x_out = self.bs_upscale(A_z_in, self.N_upscale)
+
+#                 # y' = mu * (1-x^2)z - x
+#                 A_y_out = mu * self.bs_mult(1 - self.bs_mult(A_x_in, A_x_in), A_z_in) - self.bs_upscale(A_x_in, self.N_upscale)
+
+#                 # z' = y
+#                 A_z_out = self.bs_upscale(A_y_in, self.N_upscale)
+
+#                 return np.array([A_x_out, A_y_out, A_z_out])
+#         else: # The non-Bernstein spline one is added for quicker comparison options with e.g. Diethelm's method (see fracnum.numerical)
+#             x, y = x_vals[0], x_vals[1]
+#             # x' = y
+#             x_out = y
+#             # y' = mu * (1-x^2)y - x
+#             y_out = -x - mu * (x**2 -1)*y
+
+#             # Forcing included here to use in e.g. Diethelm or another numerical piece
+#             if 'A' in self.params and 'omega' in self.params:
+#                 A = float(self.params['A'])
+#                 omega = float(self.params['omega'])
+#                 if A != 0 and omega != 0:
+#                     # TODO: CHANGE TO GENERAL ELEMENT ADDITION
+#                     y_out+=A*np.sin(omega*t_vals)
+
+#             if 'c' in self.params.keys():
+#                 c = self.params['c']
+#                 if c != 0:
+#                     # TODO: CHANGE TO GENERAL ELEMENT ADDITION
+#                     y_out+=c
+
+#             return np.array([x_out, y_out]).T
+                    
 class NegExp(ODEFunctions):
     def __init__(self, params = {}, bernstein=False, transpose = False):
         super().__init__(params, bernstein, transpose)

fracnum/plotting_utils.py
@@ -15,7 +15,7 @@ from matplotlib.collections import LineCollection
 
 from matplotlib import rc
 
-mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["lightseagreen", "orange", "limegreen"]) 
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["lightseagreen", "orange", "darkmagenta", "dodgerblue"]) 
 
 font_families = ['Segoe UI', 'Arial', 'Helvetica']
 rc('font',**{'family':'sans-serif','sans-serif':font_families})
@@ -104,7 +104,7 @@ def get_lin_line_colors(x, cmap_name = 'magma_r', cmap_trunc = [0.2, 0.8]):
     return colors, cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=np.min(x), vmax=np.max(x)), cmap=cmap)
 
 class VdP_Plotter():
-    def __init__(self, x, xder, t, params, alpha, dt, T, n_eval, comp_time, forcing_params = None, lims_override = None, cmap_name="magma_r", cmap_trunc = [0.15, 0.75]):
+    def __init__(self, x, xder, t, params, alpha, dt, T, n_eval, comp_time, forcing_params = None, lims_override = None, cmap_name="magma_r", cmap_trunc = [0.15, 0.75], beta = 1):
         self.x = x
         self.xder = xder
         self.t = t
@@ -114,8 +114,11 @@ class VdP_Plotter():
         self.T = T
         self.n_eval = n_eval
         self.comp_time = comp_time
-        self.forcing_params = forcing_params[0] # Take just the first element in this case. Can be made more general
-
+        if forcing_params is not None:
+            self.forcing_params = forcing_params[0] # Take just the first element in this case. Can be made more general
+        else:
+            self.forcing_params = None
+        self.beta = beta
         self.cmap_name = cmap_name
         self.cmap_trunc = cmap_trunc
 
@@ -160,7 +163,7 @@ class VdP_Plotter():
             fractional_settings_string = ""
             fractional_string = ""
         else:
-            fractional_settings_string = r"\alpha="+str(self.alpha)+r", "
+            fractional_settings_string = rf"\alpha={ self.alpha}, \beta = {self.beta},"
             
         if self.params['mu'] == 0:
             # If mu = 0, the damping alpha does not affect anything
@@ -207,7 +210,7 @@ class VdP_Plotter():
             label_text = ax_phase.text(0.88, 0.075, add_text, transform=ax_phase.transAxes)
 
         if save_filepath is not None:
-            plt.savefig(save_filepath, dpi=300)
+            plt.savefig(save_filepath, dpi=400)
 
         return ax_phase
 
@@ -284,15 +287,22 @@ class VdP_Plotter():
         if save_filepath is not None:
             plt.savefig(save_filepath, dpi = 300)
 
-    def phase_fourier(self, save_filepath=None):
-        grid = [
-            ['phase', 'fourier_x'],
-            ['phase', 'fourier_xder']
-        ]
+    def phase_fourier(self, save_filepath=None, x_signal = True, t_cutoff = 100):
+        if not x_signal:
+            grid = [
+                ['phase', 'fourier_x'],
+                ['phase', 'fourier_xder']
+            ]
+            title_string = "\nPhase portrait and fourier spectrum of " + self.title + "\n" + self.param_desciption
+        else:
+            grid = [
+                ['phase', 'x_signal'],
+                ['phase', 'fourier_x']
+            ]
+            title_string = "\nPhase portrait, signal and fourier spectrum of " + self.title + "\n" + self.param_desciption
         
         fig, axs = plt.subplot_mosaic(grid, figsize = self.hd_size, layout="constrained")
-
-        plt.suptitle("\nPhase portrait and fourier spectrum " + self.title + "\n" + self.param_desciption)
+        plt.suptitle(title_string)
 
         # TODO: MAKE MODULAR!
         # PHASE #
@@ -313,7 +323,9 @@ class VdP_Plotter():
 
         # Fourier #
 
-        f_x, f_xder = fft(self.x), fft(self.xder)
+        f_x = fft(self.x)
+        if not x_signal:
+            f_xder = fft(self.xder)
 
         N = len(self.x)
         T_samplespacing = self.T/(N)*(1/(2*np.pi))
@@ -328,19 +340,39 @@ class VdP_Plotter():
             omega = self.forcing_params['omega']
             axs['fourier_x'].axvline(x = omega, color = 'red', linestyle = '--', linewidth = 1.5, label = 'Forcing')
             axs['fourier_x'].legend()
-            axs['fourier_xder'].axvline(x = omega, color = 'red', linestyle = '--', linewidth = 1.5)
+            if not x_signal:
+                axs['fourier_xder'].axvline(x = omega, color = 'red', linestyle = '--', linewidth = 1.5)
 
         axs['fourier_x'].plot(xf[1:N//select_div], 2.0/N * np.abs(f_x[1:N//select_div]))
         axs['fourier_x'].set(ylabel=r"$x$ amplitude")
 
-        axs['fourier_xder'].plot(xf[1:N//select_div], 2.0/N * np.abs(f_xder[1:N//select_div]))
-        axs['fourier_xder'].set(ylabel=r"$\dot{x}$ amplitude")
-
-        axs['fourier_xder'].set_xlabel(r"Frequency ($2\pi t$)")
+        if x_signal:
+            axs['x_signal'].plot(self.t, self.x)
+            axs['x_signal'].set(ylabel=r"$x$")
+            axs['x_signal'].set_xlabel(r"$t$")
+            axs['fourier_x'].set_xlabel(r"Frequency ( $2\pi / T$ )")
+        else:
+            axs['fourier_xder'].plot(xf[1:N//select_div], 2.0/N * np.abs(f_xder[1:N//select_div]))
+            axs['fourier_xder'].set(ylabel=r"$\dot{x}$ amplitude")
+            axs['fourier_xder'].set_xlabel(r"Frequency ( $2\pi / T$ )")
 
         if self.fourier_amp_lim is not None:
             axs['fourier_x'].set_ylim(self.fourier_amp_lim)
-            axs['fourier_xder'].set_ylim(self.fourier_amp_lim)
+            if x_signal:
+                axs['x_signal'].set_ylim(self.x_lims)
+            else:
+                axs['fourier_xder'].set_ylim(self.fourier_amp_lim)
+            
+        if x_signal:
+            if t_cutoff is not None:
+                margin, top_freq = axs['fourier_x'].get_xlim()
+                max_freq = top_freq - np.abs(margin)
+
+                t_max = min(np.max(self.t), t_cutoff)
+                x_margin = np.abs(margin) / max_freq * t_max
+                
+                new_lims = (-x_margin, t_max+x_margin)
+                axs['x_signal'].set_xlim(new_lims)
 
         margin_pct_padding = 0.02
         engine = fig.get_layout_engine()

fracnum/splines/bernstein_splines.py
@@ -32,7 +32,7 @@ class BernsteinSplines:
         self.B_b = BernsteinMethods.build_bernstein_binom_basis(self.n)
 
         # Initialize storage dictionaries of spline multiplication and scaling
-        self.C_storage, self.upscale_storage = {}, {}
+        self.C_storage, self.upscale_storage, self.downscale_storage = {}, {}, {}
 
         # Initialize solution storage
         self.x_storage = None
@@ -63,7 +63,12 @@ class BernsteinSplines:
             elif knot_sel[0] == 'at':
                 # Up to selected knot
                 knot_index = knot_sel[1]
-                int = A[knot_index:(knot_index+1)]
+                if len(A.shape) == 1:
+                    int = np.reshape(A, (1, A.shape[0]))
+                else:
+                    int = A[knot_index:(knot_index+1)]
+            int = self.splines_downscale(int, self.n_eval)
+            # breakpoint()
         else:
             # Get or create the integration basis
             if alpha not in self.B_I.keys():
@@ -146,6 +151,17 @@ class BernsteinSplines:
         result = self.splines_multiply(A, scale_matrix)
         return result
     
+    def splines_downscale(self, A, n_target):
+        m = A.shape[-1]-1 # original order
+        key = (n_target, m)
+        if key in self.downscale_storage.keys():
+            P = self.downscale_storage[key]
+        else:
+            P = BernsteinMethods.bernstein_projection_matrix(m, n_target)
+            self.downscale_storage[key] = P
+        # breakpoint()
+        return np.einsum('nm,km->kn', P, A) # m: original. n: target. k: no. of knots
+    
     def build_and_save_integral_basis(self, alpha_vals, verbose = False, time_verbose = False):
         if self.silent_mode:
             verbose, time_verbose = False, False
@@ -156,3 +172,4 @@ class BernsteinSplines:
     def initialize_solver(self, f, x_0, alpha_vals, beta_vals = 1,forcing_params = {}):
         return SplineSolver(self, f, x_0, alpha_vals, beta_vals = beta_vals, forcing_parameters = forcing_params)
         
+    

fracnum/splines/spline_solvers.py
@@ -1,4 +1,5 @@
 from .static_spline_methods import SplineMethods
+from .static_bernstein_methods import BernsteinMethods
 from fracnum.numerical import sin_I_a
 import numpy as np
 from scipy.special import gamma
@@ -110,15 +111,25 @@ class SplineSolver():
 
         return x
 
-    def build_results_dict(self, x, n_tot_it, norm,it_norm, total_time, f_0, T = None, delta = None):
+    def build_results_dict(self, x, n_tot_it, norm,it_norm, total_time, f_0, T = None, delta = None, t_eval = None):
         # TODO: maybe change this to an object or xr / pandas?
         # For now: creates a dictionary of main results
 
         N_knots = x[0, :, :].shape[0] # SSOT: can be computed from x
+        if t_eval is not None:
+            x_vals = np.array([
+                # SplineMethods.a_to_vector(x[i, :, :])
+                t_eval**(self.gamma[i]-1)*\
+                BernsteinMethods.eval_t(x[i, :, :], self.bs.t_knot_vals, t_eval, der= False)
+                  for i in range(self.d)
+                ])
+        else:
+            x_vals = np.array([SplineMethods.a_to_vector(x[i, :, :]) for i in range(self.d)]).T, #TODO: transpose? CHECK!
+
         output_dict = {
             't': self.bs.t_eval_vals_list,
             # Below looks a bit confusing, but the logic is: a is the ordered coefficients and x the "flat" evaluation
-            'x': np.array([SplineMethods.a_to_vector(x[i, :, :]) for i in range(self.d)]).T, #TODO: transpose? CHECK!
+            'x': x_vals,
             'a': x, 
             'norm_type':norm,
             'norm_value':it_norm,
@@ -159,7 +170,7 @@ class SplineSolver():
         # Summary of selected results per knot
         print(f"~ {res['n_it_per_knot']:.2f} it/knot avg., {res['time_per_knot']:.6f} s/knot avg.")
 
-    def iterate_local(self, x, conv_tol, conv_max_it, div_treshold, norm, verbose):
+    def iterate_local(self, x, conv_tol, conv_max_it, div_treshold, norm, verbose, new_hilf = True):
         ### "Local" iteration iterates knot-for knot ###
 
         N_knots = self.bs.t_eval_vals_ord.shape[0] # Number of knots
@@ -181,7 +192,8 @@ class SplineSolver():
                 for k in range(self.d):
                     # Int_vals_base keeps track of the influence of the integral of all previous knots
                     # Hence, will not change in the next steps
-                    int_vals_base[k, :] = self.bs.I_a(f_no_last[k, :, :], alpha = self.alpha[k], knot_sel = ('to', i_knot))
+                    if self.alpha[k] != 0:
+                        int_vals_base[k, :] = self.bs.I_a(f_no_last[k, :, :], alpha = self.alpha[k], knot_sel = ('to', i_knot))
 
                     # EXPERIMENT!
                     # Take the previous knot's final value as an initial guess for the to-be-calculated one
@@ -193,18 +205,31 @@ class SplineSolver():
                 x_prev = x.copy() # Saves previous estimate for convergence statistics
 
                 # Compute function values f(t_M, x_M)
-                f_a_vals_tot[:, i_knot:(i_knot+1), :] = self.f(self.bs.t_eval_vals_ord[i_knot, :], x[:, i_knot:(i_knot+1), :])
+                if self.hilfer and new_hilf:
+                    t_gamma_vals = np.zeros([self.d, 1, n_eval])
+                    for k in range(self.d):
+                        t_gamma_vals[k, :, :] = self.bs.t_eval_vals_ord[i_knot, :]**(self.gamma[k]-1)
 
+                    # breakpoint()
+                    f_a_vals_tot[:, i_knot:(i_knot+1), :] = self.f(self.bs.t_eval_vals_ord[i_knot, :], x[:, i_knot:(i_knot+1), :] * t_gamma_vals)
+                else:
+                    f_a_vals_tot[:, i_knot:(i_knot+1), :] = self.f(self.bs.t_eval_vals_ord[i_knot, :], x[:, i_knot:(i_knot+1), :])
+                    
                 ### Compute integration for each element ###
                 for k in range(self.d):
                     # Get I^alpha { f(t_M, x_M) }
                     int_vals = self.bs.I_a(f_a_vals_tot[k, i_knot, :], alpha = self.alpha[k], knot_sel = ('at', i_knot))
                     # x_{M,j+1} = x_0 + _0I_t^alpha f(t_{M, j}, x_{M, j})
                     if self.hilfer:
-                        ic_part = self.hilfer_vals[k][i_knot, :] 
+                        if new_hilf: # NEW:
+                            ic_part = self.x_0[k]/gamma(self.gamma[k])
+                            x[k, i_knot, :] = ic_part + self.bs.t_eval_vals_ord[i_knot, :]**(1-self.gamma[k])*( int_vals_base[k, :] + int_vals )
+                        else: # OLD:
+                            ic_part = self.x_0[k] * self.hilfer_vals[k][i_knot, :]  
+                            x[k, i_knot, :] = ic_part + int_vals_base[k, :] + int_vals
                     else:
                         ic_part = self.x_0[k] 
-                    x[k, i_knot, :] = ic_part + int_vals_base[k, :] + int_vals
+                        x[k, i_knot, :] = ic_part + int_vals_base[k, :] + int_vals
                     # Add forcing values if selected
                     if np.array(self.forcing_vals[k]).size > 1:
                         x[k, i_knot, :] += self.forcing_vals[k][i_knot, :]
@@ -229,7 +254,11 @@ class SplineSolver():
                 # Notify if max iterations is reached
                 if conv_it == (conv_max_it - 1):
                     print(f"WARNING: max iterations ({conv_max_it}) reached on one knot, increment norm {it_norm}")
-
+        # NEW HILFER!
+        #### 2025-02-27 tried not to do this and instead just do afterwards at eval ####
+        # if self.hilfer and new_hilf:
+        #     for k in range(self.d):
+        #         x[k, :, :] = x[k, :, :] * self.bs.t_eval_vals_ord**(self.gamma[k]-1)
         return x, f_a_vals_tot, n_tot_it, it_norm
     
     def iterate_global(self, x, conv_tol, conv_max_it, div_treshold, norm, verbose, bvp = None, T = None):
@@ -294,12 +323,14 @@ class SplineSolver():
             if n_tot_it == (conv_max_it - 1):
                 print(f"WARNING: max iterations ({conv_max_it}) reached on one knot, increment norm {it_norm}")
 
-        bvp_test = self.bs.I_a_scalar(T, x[0, :, :], 1-self.gamma[0])
-        print('bvp_test, _0I^(gamma-1)_T x(t) = ', bvp_test)
+        if (1-self.gamma[0]) != 0:
+            bvp_test = self.bs.I_a_scalar(T, x[0, :, :], 1-self.gamma[0])
+            print('bvp_test, _0I^(gamma-1)_T x(t) = ', bvp_test)
 
         return x, f_a_vals, n_tot_it, it_norm
 
     def run(self, 
+            t_eval = None,
             method='local', 
             bvp = False, 
             T = None, 
@@ -339,7 +370,9 @@ class SplineSolver():
         # Gets the value for f_0. Can be useful for checking and period selection (e.g. y'(0)=0)
         f_0 = f_a_vals[:, 0, 0] 
         # Build all results here
-        result_dict = self.build_results_dict(x_sol, n_tot_it,norm, it_norm, total_time, f_0, T = T, delta = delta)
+        result_dict = self.build_results_dict(
+            x_sol, n_tot_it,norm, it_norm, total_time, f_0, T = T, delta = delta, t_eval = t_eval
+        )
         # Get a nice human-readable summary of run time statistics
         if verbose:
             SplineSolver.print_summary(result_dict)        

fracnum/splines/static_bernstein_methods.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.special import binom
+from scipy.special import binom, comb
 
 class BernsteinMethods:
     @staticmethod
@@ -24,3 +24,47 @@ class BernsteinMethods:
                 if k <= n + m:
                     C[k, i, j] = (binom(n, i) * binom(m, j)) / binom(n+m, k)
         return C
+    
+    @staticmethod
+    def bernstein_projection_matrix(m, n):
+        """
+        Compute the projection matrix to downscale Bernstein coefficients
+        from degree m (high) to degree n (low).
+        """
+        assert n < m, "Target degree n must be less than source degree m."
+        P = np.zeros((n + 1, m + 1))
+        for i in range(n + 1):
+            for j in range(m + 1):
+                P[i, j] = comb(m, j) * comb(j, i) * comb(m - j, n - i) / comb(m, n)
+        return P
+    
+    @staticmethod
+    def eval_t(A, t_knot_vals, t_eval, der=False):
+        n = A.shape[1] - 1
+        k_vals = np.arange(n+1)
+        B = BernsteinMethods.build_bernstein_binom_basis(n)
+
+        if np.isscalar(t_eval):
+            t_eval = np.reshape(t_eval, [1])
+        t_eval = np.array(t_eval)
+
+        t_output = np.zeros(t_eval.shape)
+        t_bounds = np.array([t_knot_vals[:-1].T, t_knot_vals[1:].T]).T
+        i = 0
+        for t in t_eval:
+            mask = (t_bounds[:,0]<=t) & (t<t_bounds[:,1])
+            if t == t_knot_vals[-1]:
+                mask[-1] = True
+            t_a, t_b = t_bounds[mask, 0], t_bounds[mask, 1]
+            s = (t - t_a) / (t_b - t_a)
+            
+            A_knot = A[mask]
+            if der == False:
+                t_output[i] = A_knot @ B @ (s ** k_vals)
+            else:
+                s_der_vals = np.zeros(n+1)
+                s_der_vals[1:] = k_vals[1:] * (s ** (k_vals[1:]-1))
+                t_output[i] = A_knot @ B @ s_der_vals / (t_b-t_a)
+            i+=1
+
+        return t_output